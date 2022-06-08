9d2bd77d11f6732a0dc7cae20eb044c09c311aaf
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-06-07 21:27:27 -0500

    started user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const {Schema, model} = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            // the idea to use the match property here came from the instructions I had for this homework assignment
+            //      and from stack overflow. (I did this for the Coding Boot Camp at WUSTL)
+            // the regular expression comes from the instructions for a previous homework assignment I had
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+        }
+    }
+)
